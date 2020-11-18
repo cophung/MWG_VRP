345ac505d7c5438f5075c63ce02825c888dd21d1
cophung <phungtheco1@gmail.com>
2020-11-18 15:57:48 +0700

    complete capacity algorithm

--- demo1/app3.js
+++ demo1/app3.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 let customers, orders;
 let indexRouteMap = [];
 let routeMap = [];
+let routeWeight = [];
+let sumRouteWeight = [];
 let bestRouter = 0;
 
 /**
@@ -18,63 +20,92 @@ let bestRouter = 0;
   }
 })();
 
-// orders = [
-//   {
-//     id: 142205,
-//     distances: [0, 10, 15, 20],
-//     order: { weight: 0 },
-//   },
-//   {
-//     id: 142188,
-//     distances: [10, 0, 35, 25],
-//     order: { weight: 7 },
-//   },
-//   {
-//     id: 142168,
-//     distances: [15, 35, 0, 30],
-//     order: { weight: 3 },
-//   },
-//   {
-//     id: 142171,
-//     distances: [20, 25, 30, 0],
-//     order: { weight: 10 },
-//   },
-// ];
+/**
+ * @function {} tinh tong quang duong
+ * @param {Array} arr indexRouteMap
+ */
+const funcSumRoute = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    bestRouter += orders[arr[i]].distances[arr[i + 1]];
+  }
+};
 
 /**
  *
  * @param {int} id id cua dia diem bat dau (depot)
+ * @param {int} capacityVehicle suc chua cua xe hang
  */
-const main = (firstId) => {
+const main = (firstId, capacityVehicle) => {
   let id = firstId;
+  let chayVongLap = true;
+  let sumCapacity = 0;
 
-  while (indexRouteMap.length < orders.length) {
+  while (chayVongLap) {
     let shortestWay = Number.MAX_VALUE;
+    let pointRemain = 0;
 
-    const order = orders.find((e) => e.id === id);
+    let order = orders.find((e) => e.id === id);
     const arrDistance = order.distances;
+    const weightOrder = order.order.weight;
+
+    if (capacityVehicle) {
+      sumCapacity += weightOrder;
+      if (sumCapacity > capacityVehicle) {
+        routeWeight.push(indexRouteMap[0]);
+        id = firstId;
+        sumCapacity = 0;
+        order = orders.find((e) => e.id === id);
+      } else {
+        routeWeight.push(weightOrder);
+      }
+    }
 
     indexRouteMap.push(orders.indexOf(order));
 
     arrDistance.forEach((e, index) => {
-      if (indexRouteMap.indexOf(index) < 0 && e < shortestWay) {
-        id = orders[index].id;
-        shortestWay = e;
+      if (indexRouteMap.indexOf(index) < 0) {
+        pointRemain++;
+        if (e < shortestWay) {
+          id = orders[index].id;
+          shortestWay = e;
+        }
       }
     });
 
-    if (shortestWay !== Number.MAX_VALUE) bestRouter += shortestWay;
+    chayVongLap = pointRemain > 0 ? true : false;
+
+    // if (shortestWay !== Number.MAX_VALUE) bestRouter += shortestWay;
   }
 
-  //Chay tu diem cuoi cung ve lai kho
+  //Update diem cuoi cung cua indexRouteMap array
   const firstIndexRoute = indexRouteMap[0];
-  const lastIndexRoute = indexRouteMap[indexRouteMap.length - 1];
-  bestRouter += orders[lastIndexRoute].distances[firstIndexRoute];
   indexRouteMap.push(firstIndexRoute);
 
+  //Update id cuoi cung cua routeMap array
   indexRouteMap.forEach((e) => routeMap.push(orders[e].id));
+
+  //Update diem cuoi cung cua routeWeight array
+  if (capacityVehicle) {
+    routeWeight.push(firstIndexRoute);
+  }
+
+  //Tinh tong trong luong cho tung chuyen va get ket qua vao sumRouteWeight array
+  sumCapacity = 0;
+  for (let i = 1; i < routeWeight.length; i++) {
+    if (routeWeight[i] === 0) {
+      sumRouteWeight.push(sumCapacity);
+      sumCapacity = 0;
+    } else sumCapacity += routeWeight[i];
+  }
+
+  //Tong quang duong di duoc
+  funcSumRoute(indexRouteMap);
 };
+
 main(142205);
-console.log("index routing", indexRouteMap);
-console.log("routing", routeMap);
-console.log("ditance", bestRouter);
+
+console.log("index routing: ", indexRouteMap);
+console.log("routing: ", routeMap);
+console.log("route weight: ", routeWeight);
+console.log("sum router weight: ", sumRouteWeight);
+console.log("ditance: ", bestRouter);
--- demo1/test.js
+++ demo1/test.js
@@ -1,4 +1,44 @@
-const a = [9, 8, 7, 6, 5];
-const b = [9, 8];
-let c = a.filter((e) => b.indexOf(e) < 0);
-console.log(c);
+// const a = [9, 8, 7, 6, 5];
+// const b = [0, 1, 0, 1];
+// a.forEach((e, index) => {
+//   if (b.indexOf(index) < 0) {
+//     console.log(e);
+//   }
+// });
+
+const fs = require("fs");
+let sum = 0;
+try {
+  let orders = JSON.parse(fs.readFileSync("../DB/db.json"));
+  orders.forEach((e, i) => {
+    console.log(i, e.order.weight);
+    sum += e.order.weight;
+  });
+} catch (err) {
+  console.log(err);
+  return;
+}
+console.log(sum);
+
+// orders = [
+//   {
+//     id: 142205,
+//     distances: [0, 10, 15, 20],
+//     order: { weight: 0 },
+//   },
+//   {
+//     id: 142188,
+//     distances: [10, 0, 35, 25],
+//     order: { weight: 7 },
+//   },
+//   {
+//     id: 142168,
+//     distances: [15, 35, 0, 30],
+//     order: { weight: 3 },
+//   },
+//   {
+//     id: 142171,
+//     distances: [20, 25, 30, 0],
+//     order: { weight: 10 },
+//   },
+// ];
